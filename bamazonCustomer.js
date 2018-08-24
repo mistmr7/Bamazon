36446e319334cd15830c4549e16537ea6e8c150f
mistmr7 <michael.mistarz@gmail.com>
2018-08-24 09:39:28 -0400

    Added comments to clarify code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@ let inquirer = require('inquirer')
 let stockQuantity
 let itemID
 
+// Define the connection to mysql
 let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -11,14 +12,18 @@ let connection = mysql.createConnection({
     database: 'bamazon'
 })
 
+// Connect to mysql
 connection.connect(function(err) {
     if (err) throw err;
     else {
+        // Log that we are connected so the user knows
         console.log(`Connected as id ${connection.threadId}`)
+        // Call function to get all of the products from the store and print them to the console
         getAllProducts()    
     } 
 })
 
+// Get all of the products from the store and print them to the console
 function getAllProducts() {
     let query = connection.query("SELECT * FROM products", function(err,res){
         if (err) throw err;
@@ -29,10 +34,12 @@ function getAllProducts() {
                 console.log(res[i].item_id + ' | ' + res[i].product_name + ' | ' +  res[i].price)
             }                
         }
+        // Ask the user questions about what they would like to buy
         askQuestions()
     })
 }
 
+// Prompt the user to answer questions related to their purchase of item id and quantity
 function askQuestions() {
     inquirer.prompt([{
     name: 'selectItems',
@@ -45,20 +52,28 @@ function askQuestions() {
     message: 'Please select the quantity you would like to buy',
     choices: 0
 }]).then(answer => {
+    // Parse the item id from a string to a number and the quantity to buy
     let itemID = parseInt(answer.selectItems)
     let itemQuantity = parseInt(answer.selectQuantity)
+
+    // Function called to run the product purchase
     function purchaseProducts() {
         let query = connection.query(
+            // sql line to pull all items where ID is selected from the above prompt
             'SELECT * FROM products WHERE item_id = ?',
             [itemID],
             function(err,res){
                 if (err) throw err;
                 else {
+                    // If we have enough stock, print the price of the purchase
                     if(res[0].stock_quantity >= itemQuantity) {
                         console.log(`Thank you for your purchase of ${res[0].product_name}! Your total today will be $${res[0].price * itemQuantity}.`)
                     } else {
+                        // Otherwise, print that we don't have enough in stock
                         console.log('Insufficient Quantity!')
                     }
+
+                    // set the variables for stockQuantity and itemID and set them to use in Updating the quantity
                     stockQuantity = res[0].stock_quantity
                     itemID = res[0].item_id
                     updateQuantity()
@@ -68,8 +83,11 @@ function askQuestions() {
         
          
     }
+
+    // Function called to update the quantity in stock
     function updateQuantity() {
         let query = connection.query(
+            // SQL query to set stock to new stock quantity where the item ID was chosen for purchase
             'UPDATE products SET stock_quantity = (SELECT stock_quantity - ?) WHERE item_id = ?',
             [itemQuantity,
              itemID],
@@ -77,15 +95,14 @@ function askQuestions() {
                 if (err) throw err;
                 else {
                     console.log('Stock Quantity updated to reflect purchase')
+                    // End the connection after the purchase
                     connection.end()
                 }
             }
         )
         console.log(query.sql)
     }
-    purchaseProducts()
-    
-   
+    purchaseProducts()   
 })
 }
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,8 @@
+// Pull in the required packages from npm
 let mysql = require('mysql')
 let inquirer = require('inquirer')
 
+// Define the connection to mysql
 let connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -9,6 +11,7 @@ let connection = mysql.createConnection({
     database: 'bamazon'
 })
 
+// Main menu to pull up for managers, prompting them to pick from a list of actions
 function runSearch() {
     inquirer
       .prompt({
@@ -24,6 +27,8 @@ function runSearch() {
             ]
       })
       .then(function(answer) {
+
+        // Switch made for each of the different choices they could make, running the functions below based on the choice
             switch (answer.action) {
             case "View Products for Sale":
                 viewProducts()
@@ -48,40 +53,57 @@ function runSearch() {
       });
 }
 
+// Function to run to show all of the items on the screen
 function viewProducts() {
-    let query = connection.query("SELECT * FROM products", function(err,res){
+    let query = connection.query(
+        // SQL query to select all products in inventory
+        "SELECT * FROM products",
+         function(err,res){
         if (err) throw err;
         else {
             console.log('Item ID | Product Name | Price | Quantity')
             console.log('==============================')
+            // Loop through all products and print their information to the screen
             for (i=0; i < res.length; i++){    
                 console.log(res[i].item_id + ' | ' + res[i].product_name + ' | ' +  res[i].price + ' | ' + res[i].stock_quantity)
                 
             }
             console.log('==============================')                
         }
+
+        // Bring the manager back to the main menu
         runSearch()
     })
 }
 
+// Function run to view any item that has less than 5 products in inventory
 function viewLowInventory() {
-    let query = connection.query("SELECT * FROM products", function(err,res){
+    let query = connection.query(
+        // SQL query to select all products
+        "SELECT * FROM products",
+         function(err,res){
         if (err) throw err;
         else {
             console.log('Item ID | Product Name | Price | Quantity')
             console.log('==============================')
+            // Loop through all of the items to check them all
             for (i=0; i < res.length; i++){
+                // If an item has less than 5 in stock, print that item to the screen
                 if (res[i].stock_quantity < 5) {    
                 console.log(res[i].item_id + ' | ' + res[i].product_name + ' | ' +  res[i].price + ' | ' + res[i].stock_quantity)
                 }
             }
             console.log('==============================')                
         }
+
+        // Bring the manager back to the main menu
         runSearch()
     })
 }
 
+// Function to update the stock quantity for a given item
 function addNewInventory() {
+    //Questions to identify the item needing updating and the quantity to update it to
     inquirer
         .prompt([{
             name: 'item',
@@ -93,9 +115,11 @@ function addNewInventory() {
             message: 'Please input the new stock quantity.'
         }])
         .then(function(answer) {
+            // Answers parsed to correct form for price and stock quantity
             let itemID = parseInt(answer.item)
             let stockQ = parseInt(answer.stock)
             let query = connection.query(
+                // SQL query to update products to stock quantity of the answer above on the item chosen above
                 "UPDATE products SET ? WHERE ?",
                 [{stock_quantity: stockQ}, {item_id: itemID}],
                 function(err,res){
@@ -105,7 +129,9 @@ function addNewInventory() {
                         console.log('Stock has been updated')
                         console.log('==============================')                
                     }
-                    runSearch()
+                
+                // Bring the manager back to the main menu
+                runSearch()
             })
             
         })
@@ -113,6 +139,7 @@ function addNewInventory() {
     
 }
 
+// Run the function to add a new product to the database. Answers to the first four questions will define the product
 function addNewProduct() {
     inquirer
     .prompt([{
@@ -133,10 +160,14 @@ function addNewProduct() {
         message: 'What quantity should be added to stock?'
     }])
     .then(function(answer) {
+        // Answers parsed to correct form for price and stock quantity
         let productPrice = parseInt(answer.price)
         let stockQuantity = parseInt(answer.stock)
         let query = connection.query(
-            "INSERT INTO PRODUCTS SET ?", {   
+            // SQL query
+            "INSERT INTO PRODUCTS SET ?", 
+            //set the parameters for the new item
+            {   
             product_name: answer.product,
             department_name: answer.department,
             price: productPrice,
@@ -145,10 +176,12 @@ function addNewProduct() {
                 if (err) {
                     throw err;
                 } else {
+                    // show that it ran correctly
                     console.log('New Product has been added to inventory')
                     console.log('==============================')                
                 }
-                runSearch()
+            // Bring the manager back to the main menu
+            runSearch()
         })
         
     })
